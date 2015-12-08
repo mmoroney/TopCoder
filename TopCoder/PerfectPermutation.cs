5f85f5b4dc5a3b2527af4aba0fd8e8050f2682f7
mmoroney <mikemoroney1722@hotmail.com>
2015-12-07 17:26:39 -0800

    Fixed test structure.

--- TopCoder/PerfectPermutation.cs
+++ TopCoder/PerfectPermutation.cs
@@ -36,12 +36,11 @@ namespace TopCoder
         [TestMethod]
         public void PerfectPermutationTest()
         {
-            PerfectPermutation perfectPermutation = new PerfectPermutation();
-            Assert.AreEqual(0, perfectPermutation.reorder(new int[] { 2, 0, 1 }));
-            Assert.AreEqual(2, perfectPermutation.reorder(new int[] { 2, 0, 1, 4, 3 }));
-            Assert.AreEqual(2, perfectPermutation.reorder(new int[] { 2, 3, 0, 1 }));
-            Assert.AreEqual(3, perfectPermutation.reorder(new int[] { 0, 5, 3, 2, 1, 4 }));
-            Assert.AreEqual(5, perfectPermutation.reorder(new int[] { 4, 2, 6, 0, 3, 5, 9, 7, 8, 1 }));
+            PerfectPermutationTestClass.PerfectPermutationTest(new int[] { 2, 0, 1 }, 0);
+            PerfectPermutationTestClass.PerfectPermutationTest(new int[] { 2, 0, 1, 4, 3 }, 2);
+            PerfectPermutationTestClass.PerfectPermutationTest(new int[] { 2, 3, 0, 1 }, 2);
+            PerfectPermutationTestClass.PerfectPermutationTest(new int[] { 0, 5, 3, 2, 1, 4 }, 3);
+            PerfectPermutationTestClass.PerfectPermutationTest(new int[] { 4, 2, 6, 0, 3, 5, 9, 7, 8, 1 }, 5);
         }
 
         private static void PerfectPermutationTest(int[] P, int expected)
