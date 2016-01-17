9e994fd960d25d822177748b845b48ba16ed3e29
mmoroney <mikemoroney1722@hotmail.com>
2016-01-17 14:19:36 -0800

    Added LargestSubsequence problem.

---
+++ TopCoder/LargestSubsequence.cs
@@ -0,0 +1,46 @@
+﻿// https://community.topcoder.com/stat?c=problem_statement&pm=12811
+
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TopCoder
+{
+    public class LargestSubsequence
+    {
+        public string getLargest(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            char largest = '\0';
+
+            for(int i = s.Length - 1; i >= 0; i--)
+            {
+                if(s[i] >= largest)
+                {
+                    sb.Insert(0, s[i]);
+                    largest = s[i];
+                }
+            }
+            return sb.ToString();
+        }
+    }
+
+    [TestClass]
+    public class LargestSubsequenceTestClass
+    {
+        [TestMethod]
+        public void LargestSubsequenceTest()
+        {
+            LargestSubsequenceTestClass.LargestSubsequenceTest("test", "tt");
+            LargestSubsequenceTestClass.LargestSubsequenceTest("a", "a");
+            LargestSubsequenceTestClass.LargestSubsequenceTest("example", "xple");
+            LargestSubsequenceTestClass.LargestSubsequenceTest("aquickbrownfoxjumpsoverthelazydog", "zyog");
+        }
+
+        private static void LargestSubsequenceTest(string s, string expected)
+        {
+            LargestSubsequence largestSubsequence = new LargestSubsequence();
+            Assert.AreEqual(expected, largestSubsequence.getLargest(s));
+        }
+    }
+}
