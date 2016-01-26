1e7e3bd111de8afcd97d5bc60aa2fcd071c1b582
mmoroney <mikemoroney1722@hotmail.com>
2016-01-25 20:21:06 -0800

    Added TheEncryptionDivTwo problem.

---
+++ TopCoder/TheEncryptionDivTwo.cs
@@ -0,0 +1,50 @@
+﻿// https://community.topcoder.com/stat?c=problem_statement&pm=10509
+
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TopCoder
+{
+    public class TheEncryptionDivTwo
+    {
+        public string encrypt(string message)
+        {
+            char next = 'a';
+            char[] cipher = new char[26];
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach(char current in message)
+            {
+                int index = current - 'a';
+                if(cipher[index] == 0)
+                {
+                    cipher[index] = next;
+                    next++;
+                }
+
+                stringBuilder.Append(cipher[index]);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+
+    [TestClass]
+    public class TheEncryptionDivTwoTestClass
+    {
+        [TestMethod]
+        public void TheEncryptionDivTwoTest()
+        {
+            TheEncryptionDivTwoTestClass.TheEncryptionDivTwoTest("hello", "abccd");
+            TheEncryptionDivTwoTestClass.TheEncryptionDivTwoTest("abcd", "abcd");
+            TheEncryptionDivTwoTestClass.TheEncryptionDivTwoTest("topcoder", "abcdbefg");
+            TheEncryptionDivTwoTestClass.TheEncryptionDivTwoTest("encryption", "abcdefghib");
+        }
+
+        private static void TheEncryptionDivTwoTest(string message, string expected)
+        {
+            TheEncryptionDivTwo theEncryptionDivTwo = new TheEncryptionDivTwo();
+            Assert.AreEqual(expected, theEncryptionDivTwo.encrypt(message));
+        }
+    }
+}
