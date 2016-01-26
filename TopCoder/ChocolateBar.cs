f147c9a477eb73689815c2f1b8f70a2a1f720c1f
mmoroney <mikemoroney1722@hotmail.com>
2016-01-25 18:14:13 -0800

    Added ChocolateBar problem.

---
+++ TopCoder/ChocolateBar.cs
@@ -0,0 +1,52 @@
+﻿// https://community.topcoder.com/tc?module=Static&d1=help&d2=sampleProblems
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TopCoder
+{
+    public class ChocolateBar
+    {
+        public int maxLength(string letters)
+        {
+            bool[] flags = new bool[26];
+            int maxLength = 0;
+
+            int i = 0;
+
+            for(int j = 0; j < letters.Length; j++)
+            {
+                while(flags[letters[j] - 'a'] == true)
+                {
+                    flags[letters[i] - 'a'] = false;
+                    i++;
+                }
+
+                flags[letters[j] - 'a'] = true;
+                maxLength = Math.Max(maxLength, j - i + 1);
+            }
+
+            return maxLength;
+        }
+    }
+
+    [TestClass]
+    public class ChocolateBarTestClass
+    {
+        [TestMethod]
+        public void ChocolateBarTest()
+        {
+            ChocolateBarTestClass.ChocolateBarTest("srm", 3);
+            ChocolateBarTestClass.ChocolateBarTest("dengklek", 6);
+            ChocolateBarTestClass.ChocolateBarTest("haha", 2);
+            ChocolateBarTestClass.ChocolateBarTest("www", 1);
+            ChocolateBarTestClass.ChocolateBarTest("thisisansrmbeforetopcoderopenfinals", 9);
+        }
+
+        private static void ChocolateBarTest(string letters, int expected)
+        {
+            ChocolateBar chocolateBar = new ChocolateBar();
+            Assert.AreEqual(expected, chocolateBar.maxLength(letters));
+        }
+    }
+}
