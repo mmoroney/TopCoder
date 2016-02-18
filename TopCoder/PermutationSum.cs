cbae328f82dff07c2016626efe0191ccce00509b
Mike Moroney <mikemoroney1722@hotmail.com>
2016-02-17 19:13:13 -0800

    Added PermutationSum problem.

---
+++ TopCoder/PermutationSum.cs
@@ -0,0 +1,56 @@
+﻿// https://community.topcoder.com/stat?c=problem_statement&pm=13585&rd=16648
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TopCoder
+{
+    public class PermutationSum
+    {
+        public int add(int n)
+        {
+            int[] counts = new int[10];
+
+            int digits = 0;
+            int sum = 0;
+            int permutations = 1;
+            int multiplier = 0;
+
+            while(n > 0)
+            {
+                digits++;
+
+                int digit = n % 10;
+                sum += digit;
+                counts[digit]++;
+
+                permutations = permutations * digits / counts[digit];
+                multiplier = multiplier * 10 + 1;
+
+                n /= 10;
+            }
+
+            return sum * multiplier * permutations / digits;
+        }
+    }
+
+    [TestClass]
+    public class PermutationSumTestClass
+    {
+        [TestMethod]
+        public void PermutationSumTest()
+        {
+            PermutationSumTestClass.PermutationSumTest(157, 2886);
+            PermutationSumTestClass.PermutationSumTest(313, 777);
+            PermutationSumTestClass.PermutationSumTest(1234, 66660);
+            PermutationSumTestClass.PermutationSumTest(5, 5);
+            PermutationSumTestClass.PermutationSumTest(12345, 3999960);
+            PermutationSumTestClass.PermutationSumTest(99999, 99999);
+        }
+
+        private static void PermutationSumTest(int n, int expected)
+        {
+            PermutationSum permutationSum = new PermutationSum();
+            Assert.AreEqual(expected, permutationSum.add(n));
+        }
+    }
+}
